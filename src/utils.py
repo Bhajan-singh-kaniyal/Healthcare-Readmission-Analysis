e5edaa3d660a388a4ffb51d1e905337b51f9317b
Bhajan-singh-kaniyal <kaniyalrahul240901@gmail.com>
2026-03-26 21:21:23 +0530

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,8 @@
+def print_insights(accuracy):
+    print("\n📊 Model Performance")
+    print(f"Accuracy: {accuracy:.2f}")
+
+    print("\n💡 Insights:")
+    print("- Patients with more visits have higher readmission risk")
+    print("- Older age groups show higher readmission")
+    print("- Longer hospital stays increase chances")
